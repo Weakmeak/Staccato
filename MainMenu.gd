a23fc5056fb47afba869ecb56e306fe11114818e
Weakmeak <ctibbett@student.neumont.edu>
2023-10-11 13:15:55 -0600

    Basic Menu functionality started

---
+++ MainMenu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_host_button_down() -> void:
+	$HostScreen.show()
+	$CenterContainer.hide()
+
+
+func _on_join_button_down() -> void:
+	$PlayerScreen.show()
+	$CenterContainer.hide()
